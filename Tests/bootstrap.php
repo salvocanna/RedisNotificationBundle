4488eecf64d83991236fba0d443931e99052ce9f
Salvo <salvo.cannamela@gmail.com>
2017-02-19 16:01:07 +0000

    Add some test glue

---
+++ Tests/bootstrap.php
@@ -0,0 +1,13 @@
+<?php
+
+if (!file_exists(__DIR__.'/../vendor/autoload.php') || !($loader = include __DIR__.'/../vendor/autoload.php')) {
+echo <<<'EOT'
+You need to install the project dependencies using Composer:
+$ wget http://getcomposer.org/composer.phar
+OR
+$ curl -s https://getcomposer.org/installer | php
+$ php composer.phar install --dev
+$ phpunit
+EOT;
+exit(1);
+}
